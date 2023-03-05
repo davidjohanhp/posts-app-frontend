06e46c07f3ba00147285c343dfec1074f40e2e56
David Johan Hasiholan Parhusip <david.johan@ui.ac.id>
2023-03-05 23:57:07 +0700

    test

--- src/components/GetAllPost.js
+++ src/components/GetAllPost.js
@@ -42,9 +42,9 @@ function GetAllPost() {
             console.log(response);
             // let actualData = response
             console.log(response);
-            setArray(response.data.posts)
+            // setArray(response.data.posts)
         })
-        
+
         axios.get(baseURL, requestOptions).then((response) => {
             console.log(response);
             // let actualData = response
